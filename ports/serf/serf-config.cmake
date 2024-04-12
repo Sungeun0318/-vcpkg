540a7fb3fcbf2e1b886013ebb022a05ba75c4cc0
rinrab <tima@chemodax.net>
2024-04-12 22:26:00 +0200

    [serf] fix different apr names on `static` and `dynamic` library linkages.

--- ports/serf/serf-config.cmake
+++ ports/serf/serf-config.cmake
@@ -11,11 +11,17 @@ if(NOT TARGET unofficial:SERF:serf)
     find_library(SERF_LIBRARY_RELEASE NAMES serf-1 PATHS "${CURRENT_PACKAGES_DIR}/lib" NO_DEFAULT_PATH)
     find_library(SERF_LIBRARY_DEBUG NAMES serf-1 PATHS "${CURRENT_PACKAGES_DIR}/debug/lib" NO_DEFAULT_PATH)
 
+    if (VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+        SET(INTERFACE_LINK_LIBRARIES "ZLIB::ZLIB;unofficial::apr::apr-1;unofficial::apr::aprapp-1;OpenSSL::SSL;OpenSSL::Crypto")
+    else()
+        SET(INTERFACE_LINK_LIBRARIES "ZLIB::ZLIB;unofficial::apr::libapr-1;unofficial::apr::libaprapp-1;OpenSSL::SSL;OpenSSL::Crypto")
+    endif()
+
     set_target_properties(unofficial:SERF:serf PROPERTIES
         INTERFACE_INCLUDE_DIRECTORIES "${SERF_INCLUDE_DIR}"
         IMPORTED_LOCATION_RELEASE "${SERF_LIBRARY_RELEASE}"
         IMPORTED_LOCATION_DEBUG "${SERF_LIBRARY_DEBUG}"
         IMPORTED_CONFIGURATIONS "Release;Debug"
-        INTERFACE_LINK_LIBRARIES "ZLIB::ZLIB;unofficial::apr::apr-1;unofficial::apr::aprapp-1;OpenSSL::SSL;OpenSSL::Crypto"
+        INTERFACE_LINK_LIBRARIES "${INTERFACE_LINK_LIBRARIES}"
     )
 endif()
