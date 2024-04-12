d33d53585621879ee30575fee67b94bcb6a0835a
rinrab <tima@chemodax.net>
2024-04-12 22:09:19 +0200

    [serf] Add finding of library dependencies and linking with them. - Find openssl, zlib, and apr libraries using `find_dependency()` function. - Add dependencies to the `INTERFACE_LINK_LIBRARIES` property for proper linking in static mode.

--- ports/serf/serf-config.cmake
+++ ports/serf/serf-config.cmake
@@ -1,5 +1,11 @@
+include(CMakeFindDependencyMacro)
+
 if(NOT TARGET SERF:serf)
     add_library(SERF:serf UNKNOWN IMPORTED)
+
+    find_dependency(OpenSSL)
+    find_dependency(ZLIB)
+    find_dependency(unofficial-apr)
 
     find_path(SERF_INCLUDE_DIR NAMES serf.h PATHS "${CURRENT_PACKAGES_DIR}/include/serf" NO_DEFAULT_PATH)
     find_library(SERF_LIBRARY_RELEASE NAMES serf-1 PATHS "${CURRENT_PACKAGES_DIR}/lib" NO_DEFAULT_PATH)
@@ -10,5 +16,6 @@ if(NOT TARGET SERF:serf)
         IMPORTED_LOCATION_RELEASE "${SERF_LIBRARY_RELEASE}"
         IMPORTED_LOCATION_DEBUG "${SERF_LIBRARY_DEBUG}"
         IMPORTED_CONFIGURATIONS "Release;Debug"
+        INTERFACE_LINK_LIBRARIES "ZLIB::ZLIB;unofficial::apr::apr-1;unofficial::apr::aprapp-1;OpenSSL::SSL;OpenSSL::Crypto"
     )
 endif()
